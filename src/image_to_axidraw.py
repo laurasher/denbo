5421f8eb36afa3e3a55fa1b328a3984061f88695
Laura Asher <laura.asher@jhuapl.edu>
2024-09-10 08:42:11 -0400

    Printed denser version

--- src/image_to_axidraw.py
+++ src/image_to_axidraw.py
@@ -45,14 +45,14 @@ aq_file = os.path.join(
 
 
 # Aquatint file to axidraw
-psm = ProgrammaticSvgManipulator(aq_file, scalar=10.7)
-psm.preview()
+psm = ProgrammaticSvgManipulator(aq_file, scalar=11)
+# psm.preview()
 psm.calc_xy_size()
 # psm.go_to_top_right()
 # psm.go_to_bottom_left()
 # psm.axidraw_xy_bounding_box()
 # psm.axidraw_calibrate()
-# psm.axidraw_xy_dots_inches()
+psm.axidraw_xy_dots_inches()
 
 
 '''
